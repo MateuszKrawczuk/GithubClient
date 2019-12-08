049bca3e83aba4b4dcce7f00ce0dde2209605cd5
Mateusz Krawczuk <mat.krawczuk@gmail.com>
2019-12-08 21:31:12 +0100

    Create ViewModelModule and add MainFragmentModule

--- app/src/main/java/pl/mateuszkrawczuk/tumblrclient/dagger/AppComponent.kt
+++ app/src/main/java/pl/mateuszkrawczuk/tumblrclient/dagger/AppComponent.kt
@@ -13,7 +13,9 @@ import javax.inject.Singleton
     modules =
     [AndroidInjectionModule::class,
         AppModule::class,
-        FragmentBindingModule::class
+        FragmentBindingModule::class,
+        ViewModelModule::class,
+        MainFragmentModule::class
     ]
 )
 interface AppComponent :
