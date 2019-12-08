c66c1c625a027e1467693366464a302906554c75
Mateusz Krawczuk <mat.krawczuk@gmail.com>
2019-12-08 20:59:25 +0100

    Move Directory and change from Trumblr to Github API

--- app/src/main/java/pl/mateuszkrawczuk/tumblrclient/dagger/MainFragmentModule.kt
+++ app/src/main/java/pl/mateuszkrawczuk/tumblrclient/dagger/MainFragmentModule.kt
@@ -2,14 +2,14 @@ package pl.mateuszkrawczuk.tumblrclient.dagger
 
 import dagger.Module
 import dagger.Provides
-import pl.mateuszkrawczuk.tumblrclient.api.TumblrAPI
-import pl.mateuszkrawczuk.tumblrclient.usecase.GetAllUseCase
-import pl.mateuszkrawczuk.tumblrclient.usecase.GetAllUseCaseImpl
+import pl.mateuszkrawczuk.tumblrclient.api.GithubAPI
+import pl.mateuszkrawczuk.tumblrclient.ui.main.usecase.GetAllUserReposUseCase
+import pl.mateuszkrawczuk.tumblrclient.ui.main.usecase.GetAllUserReposUseCaseImpl
 
 @Module
 object MainFragmentModule {
 
     @Provides
-    fun provideGetAllUseCase(tumblrAPI: TumblrAPI): GetAllUseCase =
-        GetAllUseCaseImpl(tumblrAPI)
+    fun provideGetAllUseCase(githubAPI: GithubAPI): GetAllUserReposUseCase =
+        GetAllUserReposUseCaseImpl(githubAPI)
 }
