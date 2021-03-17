1dc5640edeea590bba5098d5cd7adc96da1c3496
Mateusz Krawczuk <mat.krawczuk@gmail.com>
2021-03-17 14:09:52 +0100

    Fix crash during launch of application.
    * Add support for databinding
    * fragment -> fragmentContainerView to avoid crashes with NavigationFramework
    * Use lifecycleScope instead of GlobalScope
    
    Signed-off-by: Mateusz Krawczuk <mat.krawczuk@gmail.com>

--- app/src/main/java/pl/mateuszkrawczuk/tumblrclient/WelcomeScreenFragment.kt
+++ app/src/main/java/pl/mateuszkrawczuk/tumblrclient/WelcomeScreenFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -21,17 +22,16 @@ class WelcomeScreenFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_welcome_screen, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    override fun onStart() {
+        super.onStart()
         launchMainMenu()
     }
 
     fun launchMainMenu() {
 
         val navController =
-            Navigation.findNavController(activity!!, R.id.nav_host_fragment)
-        GlobalScope.launch {
+            Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+        viewLifecycleOwner.lifecycleScope.launch {
             delay(2200L)
             navController.navigate(R.id.action_welcomeScreenFragment_to_mainMenuFragment2)
         }
